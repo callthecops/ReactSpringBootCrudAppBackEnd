135aa40364a24ea0b8d336faddc8ef0f1ca68f25
callthecops <tudor_rimbetiu@yahoo.com>
2021-01-01 23:38:04 +0200

    backend

---
+++ src/main/java/com/example/CarDatabase/controller/HomeController.java
@@ -0,0 +1,9 @@
+package com.example.CarDatabase.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+}
