da9c05019b73173ff54f5345fa9c79360eba9c50
callthecops <tudor_rimbetiu@yahoo.com>
2021-01-04 00:08:17 +0200

    added entity

--- src/main/java/com/example/CarDatabase/controller/CarController.java
+++ src/main/java/com/example/CarDatabase/controller/CarController.java
@@ -1,9 +1,19 @@
 package com.example.CarDatabase.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-@RestController
+@Controller
 @RequestMapping("/cars")
 public class CarController {
+
+    @PostMapping("/submit")
+    public void submitNewCar(@RequestParam("image")MultipartFile file, @RequestParam("mark") String mark,
+                             @RequestParam("model")String model,@RequestParam("year")String year,
+                             @RequestParam("fuel")String fuel,@RequestParam("price")String price){
+
+
+    }
+
 }
