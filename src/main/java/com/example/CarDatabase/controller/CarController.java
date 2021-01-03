4850858741e541a611a403f4b06f682c25bd9e81
callthecops <tudor_rimbetiu@yahoo.com>
2021-01-04 01:12:35 +0200

    added DB

--- src/main/java/com/example/CarDatabase/controller/CarController.java
+++ src/main/java/com/example/CarDatabase/controller/CarController.java
@@ -1,5 +1,6 @@
 package com.example.CarDatabase.controller;
 
+import com.example.CarDatabase.model.Car;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +14,11 @@ public class CarController {
                              @RequestParam("model")String model,@RequestParam("year")String year,
                              @RequestParam("fuel")String fuel,@RequestParam("price")String price){
 
+
+        Car myCar = new Car(mark,model,year,fuel,price,file);
+        System.out.println(myCar.getFuel());
+        System.out.println(myCar.getMark());
+        System.out.println(myCar.getModel());
 
     }
 
