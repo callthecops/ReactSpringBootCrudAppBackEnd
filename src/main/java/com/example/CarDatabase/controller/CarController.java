3a0516502be956e36cd0f8dcf78bd27af2ca9beb
callthecops <tudor_rimbetiu@yahoo.com>
2021-01-04 23:55:21 +0200

    getAll

--- src/main/java/com/example/CarDatabase/controller/CarController.java
+++ src/main/java/com/example/CarDatabase/controller/CarController.java
@@ -3,25 +3,40 @@ package com.example.CarDatabase.controller;
 import com.example.CarDatabase.model.Car;
 import com.example.CarDatabase.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+import java.util.List;
+
+//Communication SERVER-CLIENT is very important in REST application so we always have to send a response back to the
+//client when a request has been processed.In order to do that we have 2 basic options.The first one is to have a
+//RestController which sends back response statuses to the client.The second one is to have a basic Controller but
+//return a ResponseEntity back from the method with a message and a response status.
+//return new ResponseEntity<String>("SucceSSFULL", HttpStatus.OK);
+
+@RestController
 @RequestMapping("/cars")
 public class CarController {
 
     @Autowired
     private CarService carService;
 
+    @CrossOrigin
     @PostMapping("/submit")
-    public void submitNewCar(@RequestPart("image")byte[] file, @RequestParam("mark") String mark,
-                             @RequestParam("model")String model,@RequestParam("year")String year,
-                             @RequestParam("fuel")String fuel,@RequestParam("price")String price){
+    public void submitNewCar(@RequestPart("image") byte[] file, @RequestParam("mark") String mark,
+                             @RequestParam("model") String model, @RequestParam("year") String year,
+                             @RequestParam("fuel") String fuel, @RequestParam("price") String price) {
 
-        Car myCar = new Car(mark,model,year,fuel,price,file);
+        Car myCar = new Car(mark, model, year, fuel, price, file);
         carService.saveCar(myCar);
-
     }
+
+    @CrossOrigin
+    @GetMapping("/getCars")
+    public List<Car> getAllCars(){
+        List<Car> carList = carService.getCarList();
+
+        return carList;
+    }
+
 
 }
